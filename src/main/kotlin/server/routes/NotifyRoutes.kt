1ec68d788355d2a3969f8dffabfce3988153c642
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2026-06-17 14:46:43 +0200

    NotifyRoutes.kt

---
+++ src/main/kotlin/server/routes/NotifyRoutes.kt
@@ -0,0 +1,46 @@
+﻿package com.mashiverse.server.routes
+
+import com.mashiverse.data.remote.dto.NotifyDto
+import com.mashiverse.discord.MashiBot
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+
+fun Application.notifyRoutes()  {
+
+    routing {
+        post("/api/mashi/release_notify") {
+            try {
+                val data = call.receive<NotifyDto>()
+                print(data)
+
+                val job = async { MashiBot.getInstance().notify(data) }
+                awaitAll(job)
+
+                call.respond(HttpStatusCode.OK)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respond(HttpStatusCode.InternalServerError, mapOf("message" to (e.message ?: "Unknown error")))
+            }
+        }
+
+        post("/api/mashi/approval_notify") {
+            try {
+                val data = call.receive<NotifyDto>()
+                print(data)
+
+                val job = async { MashiBot.getInstance().notify(data, isRelease = false) }
+                awaitAll(job)
+
+                call.respond(HttpStatusCode.OK)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+    }
+}
