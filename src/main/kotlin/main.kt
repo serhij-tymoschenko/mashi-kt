977b020c1c09a0d7771e9623acafe842da968ab0
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2026-06-17 14:56:58 +0200

    Enabled kord

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,21 +12,21 @@ import kotlinx.coroutines.launch
 
 @OptIn(PrivilegedIntent::class)
 suspend fun main(args: Array<String>) {
-//    val token = BuildConfig.DISCORD_TOKEN
-//    val kord = Kord(token)
-//
-//    val bot = MashiBot.initialize(kord)
-//    bot.setup()
-//
-//    CoroutineScope(Dispatchers.Default).launch {
-//        kord.login {
-//            intents = Intents {
-//                +Intent.Guilds
-//                +Intent.GuildMembers
-//                +Intent.GuildMessageReactions
-//            }
-//        }
-//    }
+    val token = BuildConfig.DISCORD_TOKEN
+    val kord = Kord(token)
+
+    val bot = MashiBot.initialize(kord)
+    bot.setup()
+
+    CoroutineScope(Dispatchers.Default).launch {
+        kord.login {
+            intents = Intents {
+                +Intent.Guilds
+                +Intent.GuildMembers
+                +Intent.GuildMessageReactions
+            }
+        }
+    }
 
     io.ktor.server.netty.EngineMain.main(args)
 }
